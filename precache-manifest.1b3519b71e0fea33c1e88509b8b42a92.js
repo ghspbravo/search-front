ca4f9dff6446e97cf8b4ddd11e71448ff368cacc
ghspbravo <gmspbravo@gmail.com>
2019-06-24 21:51:07 +0500

    Updates

---
+++ precache-manifest.1b3519b71e0fea33c1e88509b8b42a92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8db4a13b30ef4af291910cf1ce64b4",
+    "url": "/search-front/index.html"
+  },
+  {
+    "revision": "948852c7fb0232c828a1",
+    "url": "/search-front/static/css/main.e26493c9.chunk.css"
+  },
+  {
+    "revision": "3f7314580aeecd67b0e5",
+    "url": "/search-front/static/js/2.892ddf7e.chunk.js"
+  },
+  {
+    "revision": "948852c7fb0232c828a1",
+    "url": "/search-front/static/js/main.58758e4d.chunk.js"
+  },
+  {
+    "revision": "ec0666b9e523c2126847",
+    "url": "/search-front/static/js/runtime~main.72b1d0fb.js"
+  }
+]);
